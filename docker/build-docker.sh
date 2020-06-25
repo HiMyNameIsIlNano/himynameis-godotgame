85a6f3586eea2839b96985703c962078d9ec6755
HiMyNameIsIlNano <kappei84gmail.com>
2020-06-25 14:32:20 +0200

    [API-IMPROVEMENT]: Remove work around from build-docker.sh to exclude files from being added to the docker image. As such .dockerignore has been fixed.

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -14,8 +14,5 @@ cd ..
 ./gradlew :backend:clean && ./gradlew :backend:build && cd ./docker
 mkdir -p build/dependency && (cd build/dependency; jar -xf ../../../backend/build/libs/*.jar)
 
-# TODO: this is a brutal hack to make things work
-rm ./build/dependency/BOOT-INF/classes/application-local.yml
-
 docker build --build-arg DEPENDENCY=build/dependency -f ./demo-dockerfile -t demo/recipe-planner .
 
