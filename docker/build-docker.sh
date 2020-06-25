05fccd7a64c70fdc067bd4c0dfb68772f44f67eb
HiMyNameIsIlNano <kappei84gmail.com>
2020-06-25 13:43:36 +0200

    [API-IMPROVEMENT]: Remove unneeded dependency to protobuf-lite. Update docker build script. Update postgres init script to use bash instead of the default shell.

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+cd ..
+./gradlew :backend:clean && ./gradlew :backend:compileJava
+./gradlew :protobuf:clean && ./gradlew :protobuf:build
+
+cd ./docker
 # the db must be running for the test to work properly
 docker-compose -f docker-compose.yml up -d demo-db
-
 # Some cleanups
 rm -rf build
 
-cd .. && ./gradlew :backend:clean && ./gradlew :backend:build && cd ./docker
+cd ..
+./gradlew :backend:clean && ./gradlew :backend:build && cd ./docker
 mkdir -p build/dependency && (cd build/dependency; jar -xf ../../../backend/build/libs/*.jar)
+
+# TODO: this is a brutal hack to make things work
 rm ./build/dependency/BOOT-INF/classes/application-local.yml
+
 docker build --build-arg DEPENDENCY=build/dependency -f ./demo-dockerfile -t demo/recipe-planner .
 
--- docker/init-db/demo-app-db.sh
+++ docker/init-db/demo-app-db.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE demo_db WITH OWNER demouser
