1cdc87a663e6fd7a911f134e6889e0ee75c0d52d
HiMyNameIsIlNano <kappei84gmail.com>
2020-07-23 12:17:25 +0200

    [ADD-UNIT-TEST]: Implement the first integration test for the gRPC service.

---
+++ backend/src/test/java/com/example/demo/grpc/GrpcIntegrationTestConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.demo.grpc;
+
+import net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration;
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerAutoConfiguration;
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({ "com.example.demo" })
+@ImportAutoConfiguration({
+        GrpcServerAutoConfiguration.class,
+        GrpcServerFactoryAutoConfiguration.class,
+        GrpcClientAutoConfiguration.class})
+public class GrpcIntegrationTestConfiguration {
+
+}
