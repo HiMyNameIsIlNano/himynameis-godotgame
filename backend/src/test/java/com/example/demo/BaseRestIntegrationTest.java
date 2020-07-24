c054f8025063a6be2a28249c585a9ed2b5495beb
HiMyNameIsIlNano <kappei84gmail.com>
2020-07-24 15:41:43 +0200

    [ADD-UNIT-TEST]: Try to fix Integration Test on Jenkins.

--- backend/src/test/java/com/example/demo/BaseRestIntegrationTest.java
+++ backend/src/test/java/com/example/demo/BaseRestIntegrationTest.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ComponentScan({ "com.example.demo" })
 @ActiveProfiles("integration-test")
 public abstract class BaseRestIntegrationTest {
 
