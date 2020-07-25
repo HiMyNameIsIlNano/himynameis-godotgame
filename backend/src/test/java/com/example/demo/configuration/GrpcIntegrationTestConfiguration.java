346c9adc18ca0a455c7323166b8d75b028f037e9
HiMyNameIsIlNano <kappei84gmail.com>
2020-07-25 09:20:41 +0200

    [ADD-UNIT-TEST]: Remove unnecessary configuration class.

--- backend/src/test/java/com/example/demo/configuration/GrpcIntegrationTestConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-// @ComponentScan({"com.example.demo"})
-public class GrpcIntegrationTestConfiguration {}
