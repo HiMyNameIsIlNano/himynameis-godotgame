cdeb20c35a2f1281d3cd6713e5fc2161e952fc22
HiMyNameIsIlNano <kappei84gmail.com>
2020-07-25 09:00:56 +0200

    [ADD-UNIT-TEST]: Try to fix Integration Test on Jenkins.

--- backend/src/test/java/com/example/demo/BaseGrpcIntegrationTest.java
+++ backend/src/test/java/com/example/demo/BaseGrpcIntegrationTest.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-import net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration;
-import net.devh.boot.grpc.server.autoconfigure.GrpcServerAutoConfiguration;
-import net.devh.boot.grpc.server.autoconfigure.GrpcServerFactoryAutoConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -11,11 +7,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = DemoApplication.class)
-@ImportAutoConfiguration({
-        GrpcServerAutoConfiguration.class,
-        GrpcServerFactoryAutoConfiguration.class,
-        GrpcClientAutoConfiguration.class
-})
 @DirtiesContext
 @ActiveProfiles({"integration-test", "no-security"})
 public abstract class BaseGrpcIntegrationTest {
