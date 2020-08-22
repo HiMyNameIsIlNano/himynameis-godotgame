140450b884af34ef4ed7d9ff6264e189f729ea9c
HiMyNameIsIlNano <kappei84gmail.com>
2020-08-22 09:23:55 +0200

    [IMPROVE-DEPENDENCY-INJECTION]: Make use of Auto-loaded Singleton to bootstrap the Dependency Injection Container. Setup the bootstrap of the SoSe.

---
+++ frontend/Com.Example.Game/Scripts/GameStartup/GameInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autofac;
+using Com.Example.Common.DependencyInjection;
+using Godot;
+
+namespace Com.Example.Game.Scripts.GameStartup
+{
+    public class GameInitializer : Node
+    {
+        
+        public override void _Ready()
+        {
+            Console.WriteLine("Creating the Beans Container");
+            DependencyInjectionFactory.Build();
+            
+            Console.WriteLine("Startup SoSe");
+            // Call SocketServerStartup
+        }
+    }
+}
