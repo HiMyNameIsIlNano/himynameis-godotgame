46973bef65642ed04121abacbb16a1197475c6b7
HiMyNameIsIlNano <kappei84@gmail.com>
2020-09-29 09:58:16 +0200

    [FIX-PROTOBUF-LINUX]: Fix output folder for DB migration scripts. Update project init script.

--- init-proj.sh
+++ init-proj.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo 'WARN: The script MUST be run from the root project folder...'
+echo 'WARN: **  Make sure that Docker is running...'
+echo 'WARN: **  Make sure to have the Lombok support installed...'
 
 # 1. definition-generator build & publishToMavenLocal
 ./gradlew :definition-generator:build
